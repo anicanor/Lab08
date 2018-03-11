fd92bb316f339b18b60f22c1c770ac31c9653c80
anicanor <31227133+anicanor@users.noreply.github.com>
2018-03-10 23:35:12 -0800

    Update and rename dstack.cpp to args.cpp

---
+++ args.cpp
@@ -0,0 +1,16 @@
+//args.cpp
+//Aaron Nicanor
+//anicanor
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+        
+	for(int i = 1; i < argc; i++){
+                
+    	cout<<argv[i]<<endl;
+	}
+	return 0;
+}
